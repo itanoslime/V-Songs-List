0e826a1b4cb8f90a95739443fb624075eea5a976
RaitoOrange <2977277015@qq.com>
2024-01-24 20:40:27 +0800

    3

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   transpileDependencies: true,
-  publicPath:'./',
-  outputDir: 'dist',
-  assetsDir: 'static',
+  base: '/VsingerDemo/',
   plugins: [
     vue(),
     AutoImport({
